6b551022dcedaf09e301eda0dfe5b581ec15d02c
tony <hireriversanthony@gmail.com>
2023-11-14 10:07:15 -0500

    more practice with LC

--- chapter_one.py
+++ chapter_one.py
@@ -3,7 +3,7 @@
 
 list_of_nums = [4, 5, 3, 45, 6, 7, 4, 32, 22]
 list_of_strings = ['hello world!!!!!','hello world?','hello world!!!','hello world?','hello world!!',]
-list_of_multiples = [24, 'hello', 22, 'world!', 50, 'this', 'is', 100]
+list_of_multiples = [24, 'hello', 22, 'world!', 50, 'this', 'is', 100, ['whats', 'up', 55], {'23', 23, 'hello world!'}]
 
 # list comprehension -> [ expression : x + context : for x in list]
 # list comprehension -> print([ expression : x + context : for x in list])
